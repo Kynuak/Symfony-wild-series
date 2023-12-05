89f6c8901575b7638d71b2be87e81f689a72c5dc
Kynuak <bapt.trouille@gmail.com>
2023-12-05 21:59:17 +0100

    Ajout des acteurs dans les news programs

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Entity\Actor;
 use App\Entity\Program;
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
@@ -20,6 +21,13 @@ class ProgramType extends AbstractType
             ->add('category', null, [
                 'class'=> Category::class, 
                 'choice_label' => 'name'
+            ])
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class, 
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false,
             ])
         ;
     }
